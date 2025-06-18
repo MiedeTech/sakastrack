9c629144a2210f10921e9a551bb397e39adb49f0
Sondepeno Ayomide <{miedeboy@gmail.com}>
2025-06-18 05:53:00 +0100

    worked on the login/register styling

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function togglePasswordVisibility() {
-          const passwordField = document.getElementById('password');
+          const passwordField = document.getElementById('password') || document.getElementById('reg-password');
           const showPasswordCheckbox = document.getElementById('showPassword');
           passwordField.type = showPasswordCheckbox.checked ? 'text' : 'password';
 } 
+
+function togglePasswordVisibility2() {
+  const passwordField = document.getElementById('reg-password');
+  const showPasswordCheckbox = document.getElementById('showPassword2');
+  passwordField.type = showPasswordCheckbox.checked ? 'text' : 'password';
+} 
+
+
+function showForm(formId) {
+    document.querySelectorAll('.form-container').forEach(form => form.classList.remove('active'));
+    document.getElementById(formId).classList.add('active');
+}
+    
